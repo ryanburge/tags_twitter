d9d739dfbc35676326abe24629ccfa69dbc366b4
Ryan Burge <ryanburge@gmail.com>
2017-04-01 12:56:43 -0500

    basic syntax

---
+++ basic_cleaning.R
@@ -0,0 +1,55 @@
+library(tidyverse)
+library(tidytext)
+library(lubridate)
+library(stringr)
+library(httr)
+library(dplyr)
+
+
+tweets <- tweets %>%
+  mutate(date = mdy(paste(substring(created_at, 5, 10), substring(created_at, 27 ,30))))
+
+reg_words <- "([^A-Za-z_\\d#@']|'(?![A-Za-z_\\d#@]))"
+tidy_tweets <- tweets %>%
+  filter(!str_detect(text, "^RT")) %>%
+  mutate(text = str_replace_all(text, "https://t.co/[A-Za-z\\d]+|http://[A-Za-z\\d]+|&amp;|&lt;|&gt;|RT|https", "")) %>%
+  unnest_tokens(word, text, token = "regex", pattern = reg_words) %>%
+  filter(!word %in% stop_words$word,
+         str_detect(word, "[a-z]"))
+
+count <- tidy_tweets %>%
+  count(word, sort=TRUE) %>%
+  filter(substr(word, 1, 1) != '#', # omit hashtags
+         substr(word, 1, 1) != '@') %>% # omit Twitter handles
+  mutate(word = reorder(word, n))
+
+count <- count[-1,]
+
+wordcloud2(count)
+
+
+tidy_bigrams <- tweets %>%
+  filter(!str_detect(text, "^RT")) %>%
+  mutate(text = str_replace_all(text, "https://t.co/[A-Za-z\\d]+|http://[A-Za-z\\d]+|&amp;|&lt;|&gt;|RT|https", "")) %>%
+  unnest_tokens(word, text, token = "regex", pattern = reg_words) %>%
+  mutate(next_word = lead(word)) %>%
+  filter(!word %in% stop_words$word, # remove stop words
+         !next_word %in% stop_words$word, # remove stop words
+         substr(word, 1, 1) != '@', # remove user handles to protect privacy
+         substr(next_word, 1, 1) != '@', # remove user handles to protect privacy
+         substr(word, 1, 1) != '#', # remove hashtags
+         substr(next_word, 1, 1) != '#',
+         str_detect(word, "[a-z]"), # remove words containing ony numbers or symbols
+         str_detect(next_word, "[a-z]")) %>% # remove words containing ony numbers or symbols
+  filter(id_str == lead(id_str)) %>% # needed to ensure bigrams to cross from one tweet into the next
+  unite(bigram, word, next_word, sep = ' ') %>%
+  select(bigram, from_user, date, id_str, user_followers_count, user_friends_count, user_location)
+
+
+bigrams <- tidy_bigrams %>%
+  count(bigram, sort=TRUE) %>%
+  filter(n >= 30) %>%
+  mutate(bigram = reorder(bigram, n))
+
+ggplot(bigrams, aes(bigram, n)) +  geom_bar(stat = "identity") 
+  
